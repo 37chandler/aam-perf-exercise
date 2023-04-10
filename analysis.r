f1df4fc696cc005f5e2d49183d8b06adaf81ba30
John Chandler <john.chandler@gmail.com>
2023-04-10 13:05:02 -0500

    Update analysis.r

--- analysis.r
+++ analysis.r
@@ -2,12 +2,12 @@ library(tidyverse,warn.conflicts = F)
 library(tidymodels,warn.conflicts = F)
 library(here)
 
-d.price <- read_tsv(paste(here(),"price_estimates.txt",sep="/"))
+d_price <- read_tsv(here("price_estimates.txt"))
 
-d.group <- read_tsv(paste(here(),"group_estimates.txt",sep="/")) %>% 
-  mutate_all(as.factor)
+d_group <- read_tsv(here("group_estimates.txt")) %>% 
+  mutate_all(as_factor)
 
 
-skimr::skim(d.price)
-skimr::skim(d.group)
+skimr::skim(d_price)
+skimr::skim(d_group)
 
